ee42e6a6da65eefb3f1392de2b058ca1434ab5c4
Diego Benavides <diegojose2322@hotmail.com>
2022-04-23 18:27:24 -0500

    Editar, cancelar y algunas validaciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -6,7 +6,10 @@ const Formulario = () => {
     const[item, setItem] = React.useState('')
     const[descripcion, setDescripcion] = React.useState('')
     const[lista, setLista] = React.useState([])
-    //const[id, setId] = React.useState('')
+    const [modoEdicion, setModoEdicion] = React.useState(false)
+    const[id, setId] = React.useState('')
+    const [error, setError] = React.useState(null)
+
     React.useEffect(()=>{
         const obtenerDatos = async () => {
             try {
@@ -28,13 +31,15 @@ const Formulario = () => {
     const guardarDatos = async(e) => {
         e.preventDefault()
 
-        //Alertas para comprobacion de campos vacios
+        //Alertas para comprobacion de campos vacios agregar
 
         if(!item.trim()){
-            return alert('Campo fruta vacío')
+            setError('Campo fruta vacío')
+            return
         }
         if(!descripcion.trim()){
-            return alert('Campo descripcion vacío')
+            setError('Campo descripcion vacío')
+            return
         }
         try {
             const db = firebase.firestore()
@@ -49,6 +54,10 @@ const Formulario = () => {
         } catch (error) {
             console.log(error)
         }
+        setModoEdicion(false)
+        setItem('')
+        setDescripcion('')
+        setError(null)
 
         //Fin alertas para comprobacion de campos vacios
         
@@ -64,6 +73,53 @@ const Formulario = () => {
             console.log(error)
         }
     }
+
+    const auxEditar = (item)=>{
+        setItem(item.nombreItem)
+        setDescripcion(item.nombreDescripcion)
+        setModoEdicion(true)
+        setId(item.id)
+    }
+
+    const editar = async e =>{
+        e.preventDefault()
+
+        //Comprobacion campos vacios editar
+        
+        if(!item.trim()){
+            setError('Campo fruta vacío')
+            return
+        }
+        if(!descripcion.trim()){
+            setError('Campo descripcion vacío')
+            return
+        }
+        
+        //Fin comprobacion campos vacios editar
+        
+        try {
+            const db = firebase.firestore()
+            await db.collection('items').doc(id).update(
+                {
+                    nombreItem:item,
+                    nombreDescripcion:descripcion
+                }
+            )
+            setModoEdicion(false)
+        } catch (error) {
+            console.log(error)
+        }
+        setItem('')
+        setDescripcion('')
+        setError(null)
+    }
+
+    const cancelar = () => {
+        setModoEdicion(false)
+        setItem('')
+        setDescripcion('')
+        setError(null)
+    }
     return(
         <div className='container mt-5'>
             <h1 className="text-center">Formulario</h1>
@@ -77,28 +133,47 @@ const Formulario = () => {
                             <li className='list-group-item' key={item.id}>
                                 <span className="lead">{item.nombreItem} - {item.nombreDescripcion}</span>
                                 <button className="btn btn-danger btn-sm float-end mx-2" onClick={()=> eliminar(item.id)}>Eliminar</button>
-                                <button className="btn btn-warning btn-sm float-end mx-2">Editar</button>
+                                <button className="btn btn-warning btn-sm float-end mx-2" onClick={()=> auxEditar(item)}>Editar</button>
                             </li>
                         ))
                         }
                     </ul>
                 </div>
                 <div className="col-4">
-                    <h4 className="text-center">Agregar furtas</h4>
-                    <form onSubmit={guardarDatos}>
+                    <h4 className="text-center">
+                        {
+                            modoEdicion ? 'Editar Elementos' : 'Agregar Elementos'
+
+                        }</h4>
+                    <form onSubmit={modoEdicion ? editar : guardarDatos}>
+                        {
+                            error ? <span className="text-danger">{error}</span> : null
+                        }
                         <input
                             className="form-control mb-2"
                             type="text"
                             placeholder='Ingrese nombre item'
                             onChange={(e) => setItem(e.target.value)}
+                            value = {item}
                         />
                         <input
                         className="form-control mb-2"
                         type="text"
                         placeholder='Ingrese Descripcion'
                         onChange={(e) => setDescripcion(e.target.value)}
-                        />
-                        <button className='btn btn-primary btn-block' type='submit'>Agregar</button>
+                        value = {descripcion}
+                        />{
+                            modoEdicion? (
+                                <>
+                                    <button className='btn btn-warning btn-block' type='submit'>Editar</button>
+                                    <button className='btn btn-danger btn-block mx-2' onClick={()=> cancelar()}>Cancelar</button>
+                                </>
+                            ):(
+                                <button className='btn btn-primary btn-block' type='submit'>Agregar</button>
+                            )
+                        }
+                        
+                        
                     </form>
                 </div>
             </div>
